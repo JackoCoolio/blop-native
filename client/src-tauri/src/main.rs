e8d09c32349085defa314cad1f5e4b7a0431c4ed
Jackson Wambolt <jtwambolt@gmail.com>
2022-05-20 15:27:25 -0500

    Implement WS client reconnection

--- client/src-tauri/src/main.rs
+++ client/src-tauri/src/main.rs
@@ -3,102 +3,52 @@
   windows_subsystem = "windows"
 )]
 
-use std::{sync::Mutex, thread};
+use futures::{lock::Mutex, SinkExt};
+use tauri::{Result, State};
 
-use futures::{executor, stream::SplitSink, FutureExt, SinkExt, StreamExt};
-use tauri::{Manager, State};
-use tokio::net::TcpStream;
-use tokio_tungstenite::{tungstenite::Message, MaybeTlsStream, WebSocketStream};
-use websocket::connect_websocket;
+use crate::websocket::{listen, WebSocketState};
 
 pub mod websocket;
 
+pub struct Config {
+  ws_url: String,
+}
+
+/// The payload that carries messages from the WebSocket server.
+/// Should line up with the WSPayload interface defined in the frontend.
 #[derive(Clone, serde::Serialize)]
 struct WSPayload {
   message: String,
 }
 
-// struct WebSocketWrite(Mutex<SplitSink<WebSocketStream<MaybeTlsStream<TcpStream>>, Message>>);
-// struct WebSocketRead(Mutex<SplitStream<WebSocketStream<MaybeTlsStream<TcpStream>>>>);
-
-struct WebSocketState {
-  write: Mutex<SplitSink<WebSocketStream<MaybeTlsStream<TcpStream>>, Message>>,
-}
-
 #[tauri::command]
-fn send_message(state: State<WebSocketState>, message: String) -> () {
-  let mut conn = state.write.lock().unwrap();
-  executor::block_on(conn.feed(message.into())).unwrap();
-  executor::block_on(conn.flush()).unwrap();
+async fn send_message(state: State<'_, WebSocketState>, message: String) -> Result<()> {
+  let mut guard = state.write.lock().await;
+
+  // unwrap option inside MutexGuard
+  let conn = match &mut *guard {
+    Some(x) => x,
+    None => panic!(""),
+  };
+
+  // todo: handle errors
+  conn.feed(message.into()).await.unwrap();
+  conn.flush().await.unwrap();
+
+  Ok(())
 }
 
 #[tokio::main]
 async fn main() {
-  let ws_stream = connect_websocket("ws://localhost:8080/ws".into()).await;
-  println!("blop: websocket connected");
-
-  // split websocket
-  let (write, mut read) = ws_stream.split();
-
   tauri::Builder::default()
-    // .manage::<WebSocketWrite>(WebSocketWrite(Mutex::from(write)))
-    // .manage::<WebSocketRead>(WebSocketRead(Mutex::from(read)))
+    .manage::<Config>(Config {
+      ws_url: "ws://localhost:8080/ws".into(),
+    })
     .manage::<WebSocketState>(WebSocketState {
-      write: Mutex::from(write),
+      write: Mutex::from(None),
     })
     .setup(|app| {
-      let handle = app.handle();
-      println!("spawning thread");
-      thread::spawn(move || loop {
-        executor::block_on(read.for_each(|message_result| async {
-          let message = match message_result {
-            Err(_) => return,
-            Ok(x) => x,
-          };
-          let data = message.into_data();
-          let str_data = std::str::from_utf8(&data).unwrap();
-          println!("back in my day we'd have this payload delivered already!");
-          handle
-            .emit_all(
-              "message",
-              WSPayload {
-                message: str_data.into(),
-              },
-            )
-            .unwrap();
-        }));
-        println!("lost connection to websocket server");
-        let state: State<WebSocketState> = handle.state();
-
-        let new_ws_stream = connect_websocket("ws://localhost:8080/ws".into())
-          .now_or_never()
-          .unwrap();
-
-        // let new_ws_stream = executor::block_on(connect_websocket("ws://localhost:8080/ws".into()));
-        let (new_write, new_read) = new_ws_stream.split();
-        read = new_read;
-
-        let mut write = state.write.lock().unwrap();
-        *write = new_write;
-
-        println!("executor finished")
-      });
-
-      // let handler = tokio::spawn(read.for_each(|message| async {
-      //   let data = message.unwrap().into_data();
-      //   println!("{:?}", &data);
-      //   let lock = app.state::<EventEmitLock>().0.lock();
-      //   app.handle().emit_all(
-      //     "message",
-      //     WSPayload {
-      //       // message: String::from(data),
-      //       message: std::str::from_utf8(&data).unwrap().into(),
-      //     },
-      //   );
-      //   drop(lock);
-      // }));
-
-      println!("blop: setup finished");
+      tokio::spawn(listen(app.handle()));
       Ok(())
     })
     .invoke_handler(tauri::generate_handler![send_message])
--- client/src-tauri/src/websocket.rs
+++ client/src-tauri/src/websocket.rs
@@ -1,29 +1,26 @@
-use futures::{
-  lock::Mutex,
-  stream::{SplitSink, SplitStream},
-  StreamExt,
-};
+use std::time::Duration;
+
+use futures::{lock::Mutex, stream::SplitSink, StreamExt};
+use tauri::{AppHandle, Manager, State};
 use tokio::net::TcpStream;
 use tokio_tungstenite::{connect_async, tungstenite::Message, MaybeTlsStream, WebSocketStream};
 use url::Url;
 
+use crate::{Config, WSPayload};
+
 pub struct WebSocketState {
-  pub write: Mutex<SplitSink<WebSocketStream<MaybeTlsStream<TcpStream>>, Message>>,
-  pub read: Mutex<SplitStream<WebSocketStream<MaybeTlsStream<TcpStream>>>>,
+  pub write: Mutex<Option<SplitSink<WebSocketStream<MaybeTlsStream<TcpStream>>, Message>>>,
 }
 
-impl WebSocketState {
-  pub async fn connect(url: String) -> WebSocketState {
-    let ws_uri = Url::parse(&url).unwrap();
+pub struct PossibleWrite {
+  pub write: Option<SplitSink<WebSocketStream<MaybeTlsStream<TcpStream>>, Message>>,
+}
 
-    let (ws_stream, _) = connect_async(ws_uri).await.expect("failed to connect");
-
-    let (write, read) = ws_stream.split();
-
-    WebSocketState {
-      write: Mutex::from(write),
-      read: Mutex::from(read),
-    }
+impl PossibleWrite {
+  pub fn new(
+    write: Option<SplitSink<WebSocketStream<MaybeTlsStream<TcpStream>>, Message>>,
+  ) -> PossibleWrite {
+    PossibleWrite { write }
   }
 }
 
@@ -33,4 +30,77 @@ pub async fn connect_websocket(url: String) -> WebSocketStream<MaybeTlsStream<Tc
   let (ws_stream, _) = connect_async(ws_uri).await.expect("failed to connect");
 
   ws_stream
+}
+
+pub async fn try_connect(
+  uri: String,
+) -> Result<WebSocketStream<MaybeTlsStream<TcpStream>>, String> {
+  let ws_uri = Url::parse(&uri).unwrap();
+
+  // todo: give up after some number of attempts
+  loop {
+    match connect_async(ws_uri.clone()).await {
+      Err(_) => {
+        tokio::time::sleep(Duration::from_secs(5)).await;
+        continue;
+      }
+      Ok((ws_stream, _)) => {
+        println!("reconnected");
+        return Ok(ws_stream);
+      }
+    }
+  }
+
+  // Err("couldn't connect to WS server".into())
+}
+
+pub async fn listen(handle: AppHandle) {
+  let state: State<WebSocketState> = handle.state();
+  let config: State<Config> = handle.state();
+  loop {
+    println!("connecting to ws server...");
+    // acquire write lock ASAP
+    let mut write = state.write.lock().await;
+
+    // try to connect again
+    let new_ws_stream = match try_connect(config.ws_url.clone()).await {
+      Err(e) => panic!("{}", e), // panic for now
+      Ok(x) => x,
+    };
+
+    let (new_write, new_read) = new_ws_stream.split();
+
+    // update websocket connection to use new stream/sink
+    let read = new_read;
+    *write = Some(new_write);
+
+    // release lock
+    drop(write);
+
+    println!("ws server connected");
+
+    // read from websocket stream until server sends close message
+    read
+      .for_each(|message_result| async {
+        let message = match message_result {
+          Err(_) => return,
+          Ok(x) => x,
+        };
+        let data = message.into_data();
+        let str_data = std::str::from_utf8(&data).unwrap();
+
+        // emit message to frontend
+        handle
+          .emit_all(
+            "message",
+            WSPayload {
+              message: str_data.into(),
+            },
+          )
+          .expect("couldn't emit message"); // not sure how this would fail without needing to panic anyway
+      })
+      .await;
+
+    println!("lost connection");
+  }
 }
